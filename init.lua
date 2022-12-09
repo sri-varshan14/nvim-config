a72ef6568df49911fe38298e73b039d07cdebdc3
sri-varshan14 <mailmeatsri14@gmail.com>
2022-12-09 22:27:30 +0000

    Initialized the init file

---
+++ init.lua
@@ -0,0 +1,19 @@
+require('plugins')
+require('settings')
+require('keymaps')
+require('tokyonight-config')
+require('nvim-treesitter-config')
+require('nvim-tree-config')
+require('nvim-lualine-config')
+require('bufferline-config')
+require('cmp-nvim-lsp-config')
+require('mason-config')
+require('telescope-config')
+require('nvim-lspconfig-config')
+require('nvim-autopair-config')
+require('catppuccin-config')
+require('formatter-nvim-config')
+require('indent-blankline-config')
+
+vim.cmd[[colorscheme tokyonight-night]]
+
